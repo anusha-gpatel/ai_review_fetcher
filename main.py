e9f99a593203d99b9c305b6efec5f6f0835a1a16
anushagpatel <anushagpatel123@gmail.com>
2025-06-10 10:21:26 -0400

    Added some code

--- main.py
+++ main.py
@@ -1,28 +1,3 @@
-# from src.fetch_data import ICLRDataCollector
-# import pandas as pd
-# import os
-# import concurrent.futures
-
-# def fetch_and_extract(year):
-#     collector = ICLRDataCollector()
-#     papers = collector.get_conference_papers(year)
-#     return [collector.extract_paper_info(paper, year, "ICLR") for paper in papers]
-
-# if __name__ == "__main__":
-#     os.makedirs("output", exist_ok=True)
-
-#     all_years = list(range(2018, 2025))
-#     all_papers = []
-
-#     with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
-#         results = executor.map(fetch_and_extract, all_years)
-#         for year_papers in results:
-#             all_papers.extend(year_papers)
-
-#     df = pd.DataFrame(all_papers)
-#     df.to_csv("output/iclr_all_years_papers.csv", index=False)
-#     print("✅ Data saved to output/iclr_all_years_papers.csv")
-
 from src.fetch_data import ICLRDataCollector
 import pandas as pd
 import os
@@ -40,4 +15,4 @@ if __name__ == "__main__":
 
     df = pd.DataFrame(all_papers)
     df.to_csv("output/iclr_all_years_papers.csv", index=False)
-    print("✅ Data saved to output/iclr_all_years_papers.csv")
+    print("Data saved to output/iclr_all_years_papers.csv")
